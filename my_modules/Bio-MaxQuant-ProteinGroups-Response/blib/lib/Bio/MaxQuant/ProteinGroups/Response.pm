078c10e39e08a03a6d21c986eaccbcc4468f2abc
Jimi Wills <jimiwills@gmail.com>
2014-03-12 09:50:58 +0000

    prerequisites

--- my_modules/Bio-MaxQuant-ProteinGroups-Response/blib/lib/Bio/MaxQuant/ProteinGroups/Response.pm
+++ my_modules/Bio-MaxQuant-ProteinGroups-Response/blib/lib/Bio/MaxQuant/ProteinGroups/Response.pm
@@ -21,11 +21,11 @@ Bio::MaxQuant::ProteinGroups::Response - Analyze MQ proteinGroups for differenti
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
